a71a5599f08815960f01c5d6e0f2a99761a515ed
fidget1 <macallister.armstrong@gmail.com>
2022-01-12 00:15:48 -0500

    login, register, landing, navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
-const App = () => 
-    <Fragment>
-      <h1>App</h1>
-    </Fragment>
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing/>} />
+        </Routes>
+        <section className="container">
+          <Routes>
+            <Route path='register' element={<Register/>} />
+            <Route path='login' element={<Login/>} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
+  );
+}
 
 export default App;
